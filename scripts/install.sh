eba11b1e77022b48980e9e004a49cea6bc6313b7
pmartin <contact@elol.fr>
2016-07-27 17:54:22 +0200

    [ci] debug path

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,16 +8,16 @@ psql $DBNAME -c "CREATE EXTENSION pgcrypto WITH SCHEMA pgcrypto;" -U postgres
 
 # Install schemas
 BASE=src
-FILES="pgproc/sql/*.sql pgproc/plpgsql/*.sql"
-FILES="$FILES portal/sql/portal.sql portal/sql/comments.sql portal/sql/mainview_*.sql portal/sql/personview_*.sql portal/plpgsql/*.sql"
-FILES="$FILES organ/sql/organ.sql organ/sql/comments.sql organ/plpgsql/*.sql"
-FILES="$FILES login/sql/auth.sql login/sql/comments.sql login/plpgsql/*.sql"
+FILES="$BASE/pgproc/sql/*.sql $BASE/pgproc/plpgsql/*.sql"
+FILES="$FILES $BASE/portal/sql/portal.sql $BASE/portal/sql/comments.sql $BASE/portal/sql/mainview_*.sql $BASE/portal/sql/personview_*.sql $BASE/portal/plpgsql/*.sql"
+FILES="$FILES $BASE/organ/sql/organ.sql $BASE/organ/sql/comments.sql $BASE/organ/plpgsql/*.sql"
+FILES="$FILES $BASE/login/sql/auth.sql $BASE/login/sql/comments.sql $BASE/login/plpgsql/*.sql"
 
 echo 'Installing SQL from files:'
 for i in $FILES; do 
     echo " - $i";
 done
-(echo 'BEGIN TRANSACTION; ' && cat $BASE/$FILES && echo 'COMMIT; ' ) |  PGPASSWORD=$DBPASS PGOPTIONS="--client-min-messages=warning" psql -v ON_ERROR_STOP=1 -q -h localhost -U $DBUSER $DBNAME
+(echo 'BEGIN TRANSACTION; ' && cat $FILES && echo 'COMMIT; ' ) |  PGPASSWORD=$DBPASS PGOPTIONS="--client-min-messages=warning" psql -v ON_ERROR_STOP=1 -q -h localhost -U $DBUSER $DBNAME
 
 #bash ./install.sh
 #bash ./update.sh
