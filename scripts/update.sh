ec5434cab18d76424f0cb417f7bd2610e0450243
pmartin <contact@elol.fr>
2016-07-28 16:39:51 +0200

    Add scripts to create db locally

---
+++ scripts/update.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+# to be run locally, as postgres user
+
+function err {
+  echo 'ERROR: '$1;
+  exit 1;
+}
+
+# you can use: sudo su postgres -c ./scripts/update.sh
+[ $(whoami) = 'postgres' ] || err "must be postgres (try: sudo su postgres -c $0)"
+
+[ -e config.sh ] && . config.sh || err 'config.sh not found';
+
+psql <<EOF
+DROP DATABASE IF EXISTS $DBNAME;
+CREATE DATABASE $DBNAME WITH ENCODING='UTF8' OWNER=$DBUSER;
+EOF
+
+PGPASSWORD=$DBPASS psql $DBNAME <<EOF
+CREATE SCHEMA pgcrypto AUTHORIZATION $DBUSER;
+CREATE EXTENSION pgcrypto WITH SCHEMA pgcrypto;
+EOF
+
+# Install schemas
+PGBASE=vendor/actimeo/pgproc/src
+BASE=src
+FILES="$PGBASE/sql/*.sql $PGBASE/plpgsql/*.sql"
+FILES="$FILES $BASE/portal/sql/portal.sql $BASE/portal/sql/comments.sql $BASE/portal/sql/mainview_*.sql $BASE/portal/sql/personview_*.sql $BASE/portal/plpgsql/*.sql"
+FILES="$FILES $BASE/organ/sql/organ.sql $BASE/organ/sql/comments.sql $BASE/organ/plpgsql/*.sql"
+FILES="$FILES $BASE/login/sql/auth.sql $BASE/login/sql/comments.sql $BASE/login/plpgsql/*.sql"
+
+echo 'Installing SQL from files:'
+for i in $FILES; do 
+    echo " - $i";
+done
+(echo 'BEGIN TRANSACTION; ' && cat $FILES && echo 'COMMIT; ' ) |  PGPASSWORD=$DBPASS PGOPTIONS="--client-min-messages=warning" psql -v ON_ERROR_STOP=1 -q -h localhost -U $DBUSER $DBNAME
+
