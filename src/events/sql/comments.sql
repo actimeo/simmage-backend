ea692d308bc4c9a37e47f1a63359fddfff62034f
pmartin <contact@elol.fr>
2016-09-30 14:34:11 +0200

    (docs) comments

--- src/events/sql/comments.sql
+++ src/events/sql/comments.sql
@@ -1,15 +1,15 @@
-COMMENT ON SCHEMA events IS '';
+COMMENT ON SCHEMA events IS 'Events';
 
-COMMENT ON TABLE events.event_type IS '';
-COMMENT ON COLUMN events.event_type.ety_id IS '';
-COMMENT ON COLUMN events.event_type.ety_category IS '';
-COMMENT ON COLUMN events.event_type.ety_name IS '';
-COMMENT ON COLUMN events.event_type.ety_individual_name IS '';
+COMMENT ON TABLE events.event_type IS 'Events types';
+COMMENT ON COLUMN events.event_type.ety_id IS 'Unique identifier';
+COMMENT ON COLUMN events.event_type.ety_category IS 'Event category';
+COMMENT ON COLUMN events.event_type.ety_name IS 'Type name';
+COMMENT ON COLUMN events.event_type.ety_individual_name IS 'Is the event name should be replaced?';
 
-COMMENT ON TABLE events.event_type_organization IS '';
-COMMENT ON COLUMN events.event_type_organization.ety_id IS '';
-COMMENT ON COLUMN events.event_type_organization.org_id IS '';
+COMMENT ON TABLE events.event_type_organization IS 'Organizations interested in events types';
+COMMENT ON COLUMN events.event_type_organization.ety_id IS 'Event type identifier';
+COMMENT ON COLUMN events.event_type_organization.org_id IS 'Organizarion identifier';
 
-COMMENT ON TABLE events.event_type_topic IS '';
-COMMENT ON COLUMN events.event_type_topic.ety_id IS '';
-COMMENT ON COLUMN events.event_type_topic.top_id IS '';
+COMMENT ON TABLE events.event_type_topic IS 'Topics related to events types';
+COMMENT ON COLUMN events.event_type_topic.ety_id IS 'Event type identifier';
+COMMENT ON COLUMN events.event_type_topic.top_id IS 'Topic identifier';
