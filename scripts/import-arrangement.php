88e609cf0b9e62b5d67b1149f7ca75101b7979ca
pmartin <contact@elol.fr>
2016-10-29 10:41:39 +0200

    (chore) add pg port to arrangement import

--- scripts/import-arrangement.php
+++ scripts/import-arrangement.php
@@ -21,7 +21,7 @@ if ($argc < 2) {
 
 $dir = $argv[1];
 
-$base = new PgProcedures ($pg_host, $pg_user, $pg_pass, $pg_database);
+$base = new PgProcedures ($pg_host, $pg_user, $pg_pass, $pg_database, $pg_port);
 
 $base->startTransaction();
 
