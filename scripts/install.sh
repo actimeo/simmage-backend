568c892c08335a92a2924db612b185ffffe433a4
pmartin <contact@elol.fr>
2016-10-29 10:31:35 +0200

    (chore) debug port

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 # Create database
-psql -c "create user $DBUSER password '$DBPASS'" -U postgres
-psql -c "CREATE DATABASE $DBNAME WITH ENCODING='UTF8' owner=$DBUSER" -U postgres
-PGPASSWORD=$DBPASS psql $DBNAME -c "CREATE SCHEMA pgcrypto AUTHORIZATION $DBUSER;"
-psql $DBNAME -c "CREATE EXTENSION pgcrypto WITH SCHEMA pgcrypto;" -U postgres
+psql -p $DBPORT -c "create user $DBUSER password '$DBPASS'" -U postgres
+psql -p $DBPORT -c "CREATE DATABASE $DBNAME WITH ENCODING='UTF8' owner=$DBUSER" -U postgres
+PGPASSWORD=$DBPASS psql -p $DBPORT $DBNAME -c "CREATE SCHEMA pgcrypto AUTHORIZATION $DBUSER;"
+psql -p $DBPORT $DBNAME -c "CREATE EXTENSION pgcrypto WITH SCHEMA pgcrypto;" -U postgres
 
 # Install schemas
 PGBASE=vendor/actimeo/pgproc/src
@@ -21,8 +21,8 @@ echo 'Installing SQL from files:'
 for i in $FILES; do 
     echo " - $i";
 done
-(echo 'BEGIN TRANSACTION; ' && cat $FILES && echo 'COMMIT; ' ) |  PGPASSWORD=$DBPASS PGOPTIONS="--client-min-messages=warning" psql -v ON_ERROR_STOP=1 -q -h localhost -U $DBUSER $DBNAME
+(echo 'BEGIN TRANSACTION; ' && cat $FILES && echo 'COMMIT; ' ) |  PGPASSWORD=$DBPASS PGOPTIONS="--client-min-messages=warning" psql -p $DBPORT -v ON_ERROR_STOP=1 -q -h localhost -U $DBUSER $DBNAME
 
 #bash ./install.sh
 #bash ./update.sh
-echo "<?php \$pg_host = 'localhost'; \$pg_user = '$DBUSER'; \$pg_pass = '$DBPASS'; \$pg_database = '$DBNAME';" > config.inc.php
+echo "<?php \$pg_host = 'localhost'; \$pg_user = '$DBUSER'; \$pg_pass = '$DBPASS'; \$pg_database = '$DBNAME'; \$pg_port = '$DBPORT';" > config.inc.php
