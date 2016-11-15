ab79a6a156218bb4a7d8a8c2497b5a0f3de58071
pmartin <contact@elol.fr>
2016-11-15 17:29:47 +0100

    (lists) lists of dossiers

--- scripts/update.sh
+++ scripts/update.sh
@@ -30,6 +30,7 @@ FILES="$FILES $BASE/organ/sql/organ.sql $BASE/organ/plpgsql/*.sql $BASE/organ/sq
 FILES="$FILES $BASE/login/sql/auth.sql $BASE/login/plpgsql/*.sql $BASE/login/sql/comments.sql"
 FILES="$FILES $BASE/events/sql/eventtype.sql $BASE/events/sql/eventsviews.sql $BASE/events/plpgsql/*.sql $BASE/events/sql/comments.sql"
 FILES="$FILES $BASE/documents/sql/documenttype.sql $BASE/documents/sql/documentsviews.sql $BASE/documents/plpgsql/*.sql $BASE/documents/sql/comments.sql"
+FILES="$FILES $BASE/lists/sql/listsviews.sql $BASE/lists/plpgsql/*.sql $BASE/lists/sql/comments.sql"
 FILES="$FILES $BASE/pgdoc/sql/schema.sql $BASE/pgdoc/plpgsql/*.sql"
 
 echo 'Installing SQL from files:'
