8c661090eddda115c2a4f4e58eda026271e594ee
pmartin <contact@elol.fr>
2016-11-22 09:06:29 +0100

    (scripts) call arranger from arrangement import

--- scripts/import-arrangement-sub/portals.php
+++ scripts/import-arrangement-sub/portals.php
@@ -1,5 +1,5 @@
 <?php
-function import_portals($file, $base, $token, $delimiter = "\t") {
+function import_portals($file, $base, $token, $delimiter = "\t", $topics_map, $arranger_path) {
   $f = fopen($file, 'r');
   if ($f === FALSE)
     return -1;
@@ -11,6 +11,11 @@ function import_portals($file, $base, $token, $delimiter = "\t") {
     $code = $line[0];
     $id = $base->portal->portal_add($token, $line[1], $line[2]);
     $code_map[$code] = $id;
+    if (isset ($line[3]) && $line[3]) {
+      $arranger = $line[3];
+      include_once($arranger_path . DIRECTORY_SEPARATOR . $arranger . '.php');
+      arranger($base, $token, $id, $topics_map);
+    }
   }
 
   fclose($f);
