206acbf6c607cf70ac7df93a0e7bba5881ca9928
pmartin <contact@elol.fr>
2016-11-28 14:32:50 +0100

    (resources) resources views

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,15 +10,26 @@ psql -p $DBPORT $DBNAME -c "CREATE EXTENSION pgcrypto WITH SCHEMA pgcrypto;" -U 
 PGBASE=vendor/actimeo/pgproc/src
 BASE=src
 FILES="$PGBASE/sql/*.sql $PGBASE/plpgsql/*.sql"
+
 FILES="$FILES $BASE/portal/sql/portal.sql $BASE/portal/plpgsql/*.sql $BASE/portal/sql/comments.sql"
+
 FILES="$FILES $BASE/organ/sql/organ.sql $BASE/organ/plpgsql/*.sql $BASE/organ/sql/comments.sql"
+
 FILES="$FILES $BASE/login/sql/auth.sql $BASE/login/plpgsql/*.sql $BASE/login/sql/comments.sql"
+
 FILES="$FILES $BASE/events/sql/eventtype.sql $BASE/events/sql/eventsviews.sql $BASE/events/sql/event.sql $BASE/events/plpgsql/*.sql $BASE/events/sql/comments.sql"
+
 FILES="$FILES $BASE/documents/sql/documenttype.sql $BASE/documents/sql/documentsviews.sql $BASE/documents/sql/document.sql"
 FILES="$FILES $BASE/documents/plpgsql/*.sql $BASE/documents/sql/comments.sql"
+
 FILES="$FILES $BASE/lists/sql/listsviews.sql $BASE/lists/plpgsql/*.sql $BASE/lists/sql/comments.sql"
+
 FILES="$FILES $BASE/notes/sql/notesviews.sql $BASE/notes/sql/note.sql"
 FILES="$FILES $BASE/notes/plpgsql/*.sql $BASE/notes/sql/comments.sql"
+
+FILES="$FILES $BASE/resources/sql/resourcesview.sql"
+FILES="$FILES $BASE/resources/plpgsql/*.sql $BASE/notes/sql/comments.sql"
+
 FILES="$FILES $BASE/pgdoc/sql/schema.sql $BASE/pgdoc/plpgsql/*.sql"
 
 echo 'Installing SQL from files:'
