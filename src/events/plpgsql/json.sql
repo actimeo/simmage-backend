9ed5858ca3dca598a851f8c03cbf91234805d0de
Jeremy Beaupuy <jbeaupuy@actimeo.fr>
2017-02-06 13:56:27 +0100

    Reimplemented changes for events form and display

--- src/events/plpgsql/json.sql
+++ src/events/plpgsql/json.sql
@@ -77,7 +77,7 @@ $$;
 COMMENT ON FUNCTION events.event_type_json (prm_token integer, prm_ety integer, req json)
   IS 'Returns an event type or the list of event types as json';
 
-CREATE OR REPLACE FUNCTION events.event_type_filter_json(prm_token integer, prm_category events.event_category, prm_top_ids integer[])
+CREATE OR REPLACE FUNCTION events.event_type_filter_json(prm_token integer, prm_category events.event_category, prm_top_ids integer[], prm_filter boolean)
 RETURNS json
 LANGUAGE plpgsql
 STABLE
@@ -94,10 +94,10 @@ BEGIN
   RETURN ret;
 END;
 $$;
-COMMENT ON FUNCTION events.event_type_filter_json(prm_token integer, prm_category events.event_category, prm_top_ids integer[])
+COMMENT ON FUNCTION events.event_type_filter_json(prm_token integer, prm_category events.event_category, prm_top_ids integer[], prm_filter boolean)
   IS 'Return a list of event types linked to a specific category and topics';
 
-CREATE OR REPLACE FUNCTION events.event_type_list_json(prm_token integer, prm_evv_id integer, prm_top_ids integer[])
+CREATE OR REPLACE FUNCTION events.event_type_list_json(prm_token integer, prm_evv_id integer, prm_top_ids integer[], prm_filter boolean)
 RETURNS json
 LANGUAGE plpgsql
 STABLE
@@ -108,9 +108,9 @@ BEGIN
   PERFORM login._token_assert(prm_token, NULL);
   SELECT array_to_json(array_agg(row_to_json(d)))
   INTO ret
-  FROM (SELECT t.cat, events.event_type_filter_json(prm_token, t.cat, prm_top_ids) as events
+  FROM (SELECT t.cat, events.event_type_filter_json(prm_token, t.cat, prm_top_ids, prm_filter) as events
 	FROM (SELECT unnest(evv_categories) as cat FROM events.eventsview WHERE evv_id = prm_evv_id) as t ) d;
   RETURN ret;
 END;
 $$;
-COMMENT ON FUNCTION events.event_type_list_json(prm_token integer, prm_evv_id integer, prm_top_ids integer[]) IS 'Return a list of event types as json grouped by categories';
+COMMENT ON FUNCTION events.event_type_list_json(prm_token integer, prm_evv_id integer, prm_top_ids integer[], prm_filter boolean) IS 'Return a list of event types as json grouped by categories';
