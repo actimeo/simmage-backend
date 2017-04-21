21eafdad1b75960c951f299a7a98a3a87c4eab40
pmartin <contact@elol.fr>
2017-04-21 15:25:39 +0200

    (tests) tests on arrangement

--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,5 +1,10 @@
-#! /bin/sh
+#! /bin/bash
 
 rm -rf .traces 
 ./vendor/bin/phpunit --stop-on-error src && ./vendor/bin/phpunit --stop-on-error tests
-php ./scripts/import.php ./arrangement-test
+rc=$?
+if [[ $rc != 0 ]]; then
+    exit $rc
+fi
+#php ./scripts/import.php ./arrangement-test
+./vendor/bin/phpunit --stop-on-error tests-arr
